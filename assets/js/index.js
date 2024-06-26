38ea7cd16c42b5518248f37a3f1e55024ddb0de6
Notch Apple <96767159+NotchApple1703@users.noreply.github.com>
2024-06-26 10:40:08 +0700

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+var typed = new Typed('#typed-greeting', {
+      strings: [
+        'Notch Apple',
+        'a Student',
+        'a Developer',
+        'a Translator',
+      ],
+      smartBackspace: true,
+      typeSpeed: 80,
+      backSpeed: 35,
+      loop: true,
+      cursorChar: '_',
+    });
