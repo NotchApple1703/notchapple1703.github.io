1009d58dca4ef7fd636bcb3afc400fb0bf19a2de
notchapple1703 <gapple1703@gmail.com>
2024-06-27 14:37:35 +0700

    feat: Changed typed info
    Changed typed info in hero section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,9 @@
 var typed = new Typed('#typed-greeting', {
       strings: [
-        'a Student',
-        'a Developer',
-        'a Translator',
+        'Student',
+        'Developer',
+        'Translator',
       ],
-      smartBackspace: true,
       typeSpeed: 80,
       backSpeed: 35,
       loop: true,
